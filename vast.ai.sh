1c109c9a9f097168c26e003deeb6fbfb967984a3
Ionatan Frim <>
2021-03-21 17:07:34 +0200

    pc-darts on other data set

--- vast.ai.sh
+++ vast.ai.sh
@@ -3,6 +3,6 @@ update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.6 1 &&
 update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.8 2 &&
 update-alternatives --config python3 &&
 python -m pip install --upgrade pip ;
-pip install -r requirements_dev.txt ;
+pip install -r requirements.txt ;
 wget https://raw.githubusercontent.com/vast-ai/vast-python/master/vast.py -O vast; chmod +x vast;
 cp vast /usr/local/bin
